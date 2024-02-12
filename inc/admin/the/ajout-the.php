6bd494e5a1c0c3134e9050c98b6ec319426b175e
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-02-12 20:31:52 +0300

    CRUD thé php

---
+++ inc/admin/the/ajout-the.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once '../../fonction.php';
+
+if (isset($_POST['nom'], $_POST['occupation'], $_POST['rendement'])) {
+    extract($_POST);
+
+    $reponse = [
+        'success' => true,
+        'message' => 'Variété de thé ajoutée'
+    ];
+
+    if (empty($nom)) {
+        $reponse = [
+            'status' => false,
+            'message' => 'Le nom de la variété de thé ne doit pas être vide'
+        ];
+    } else if ($occupation <= 0) {
+        $reponse = [
+            'status' => false,
+            'message' => 'Le terrain occupé par un pied de thé doit être supérieur à 0'
+        ];
+    } else if ($rendement <= 0) {
+        $reponse = [
+            'status' => false,
+            'message' => 'Le rendement mensuel d\'un pied de thé doit être supérieur à 0'
+        ];
+    } else {
+        createTeaThe($nom, $occupation, $rendement);
+    }
+    
+    echo json_encode($reponse);
+}
+
+?>
