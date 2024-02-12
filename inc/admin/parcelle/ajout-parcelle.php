e94b79e4883263ab99f8285b7153700a934c392f
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-02-12 21:00:05 +0300

    CRUD parcelle php

---
+++ inc/admin/parcelle/ajout-parcelle.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '../../fonction.php';
+
+if (isset($_POST['idThe'], $_POST['nom'], $_POST['surface'])) {
+    extract($_POST);
+
+    $reponse = [
+        'success' => true,
+        'message' => 'Parcelle ajoutée'
+    ];
+
+    if (empty($nom)) {
+        $reponse = [
+            'status' => false,
+            'message' => 'Le nom de la parcelle ne doit pas être vide'
+        ];
+    } else if ($surface <=  0) {
+        $reponse = [
+            'status' => false,
+            'message' => 'La surface de la parcelle doit être supérieure à  0'
+        ];
+    } else {
+        createParcelle($idThe, $nom, $surface);
+    }
+    
+    echo json_encode($reponse);
+}
+
+?>
---
+++ inc/admin/parcelle/modif-parcelle.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '../../fonction.php';
+
+if (isset($_POST['id'], $_POST['idThe'], $_POST['nom'], $_POST['surface'])) {
+    extract($_POST);
+
+    $reponse = [
+        'success' => true,
+        'message' => 'Parcelle modifiée'
+    ];
+
+    if (empty($nom)) {
+        $reponse = [
+            'status' => false,
+            'message' => 'Le nom de la parcelle ne doit pas être vide'
+        ];
+    } else if ($surface <=  0) {
+        $reponse = [
+            'status' => false,
+            'message' => 'La surface de la parcelle doit être supérieure à  0'
+        ];
+    } else {
+        updateParcelle($id, $idThe, $nom, $surface);
+    }
+    
+    echo json_encode($reponse);
+}
+
+?>
