c9fc37b9c59060419fa7734e1bd799fc6c0b7c4f
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-02-13 17:34:52 +0300

    Adaptation au serveur d'ITU

--- inc/route.php
+++ inc/route.php
@@ -1,6 +1,6 @@
 <?php 
 
-define('SITE_NAME', 'web-s3');
+define('SITE_NAME', 'ETU002547/t/web-s3-tea');
 define('MAIN_URL', 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/' . SITE_NAME . '/');
 define('ASSETS', MAIN_URL."assets/");
 define('CSS', ASSETS . 'css/');
